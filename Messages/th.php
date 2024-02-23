db82589640acfc9c05a6330730309fc0fbfa5f24
Weblate <noreply@weblate.org>
2024-02-23 05:56:53 +0300

    Added translation using Weblate (Thai)

---
+++ Messages/th.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    /**
+ * Copyright (C) MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nikolay Beketov, 6 2018
+ *
+ */
+    'repModuleQueueCallBack' => '',
+    'mo_ModuleModuleQueueCallBack' => '',
+    'BreadcrumbModuleQueueCallBack' => '',
+    'SubHeaderModuleQueueCallBack' => '',
+    'module_queue_callback_general' => '',
+    'module_queue_callback_queues' => '',
+    'module_queue_annonce_order_ok' => '',
+    'module_queue_call_back_alertForUser' => '',
+    'module_queue_call_backDelay' => '',
+    'module_queueCallback_AddNewRecord' => '',
+    'module_queue_call_backNumberFieldLabel' => '',
+    'module_queue_call_backBillsecFieldLabel' => '',
+    'module_queue_call_backCountCallsFieldLabel' => '',
+    'module_queue_call_back_delta_no_answered_calls' => '',
+];
